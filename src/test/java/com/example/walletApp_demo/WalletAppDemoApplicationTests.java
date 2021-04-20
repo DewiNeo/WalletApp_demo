bbbe01c76c9001f665f85d2ce3939ba401af117c
meimei <dewi.ismaneti@binus.ac.id>
2021-04-20 13:57:17 +0700

    inisialisasi_walletApp_demo

---
+++ src/test/java/com/example/walletApp_demo/WalletAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletApp_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
